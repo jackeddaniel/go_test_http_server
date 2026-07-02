cef1d2aec4592afa2cbd037f91372b4b54c3276f
jackeddaniel <siddhant.ugarkar@gmail.com>
2026-07-02 13:12:33 +0530

    basic server yayyyy

--- main.go
+++ main.go
@@ -2,33 +2,16 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"log"
+	"net/http"
 )
 
-type Page struct {
-	Title string
-	Body  []byte
-}
-
-// we make a method to make the page persistent
-func (p *Page) save() error {
-	filename := p.Title + ".txt"
-	return os.WriteFile(filename, p.Body, 0600)
-}
-
-func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
-	body, err := os.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-	return &Page{Title: title, Body: body}, nil
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	log.Println(r.URL)
 }
 
 func main() {
-	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page. \n Let's get started.")}
-	p1.save()
-
-	p2, _ := loadPage("TestPage")
-	fmt.Println(string(p2.Body))
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
